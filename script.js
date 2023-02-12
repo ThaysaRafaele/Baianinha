0e552acde9fc992adb4f6bcda4585d8847623ceb
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-02-12 16:45:52 -0400

    alterar quantidade e tamanho do produto

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 /************** DECLARANDO CONSTANTES ***************/
-
 const seleciona = (elemento) => document.querySelector(elemento);
 const selecionaTodos = (elemento) => document.querySelectorAll(elemento);
+
+const formatoReal = (valor) => {
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
+
+const formatoMonetario = (valor) => {
+    if(valor) {
+        return valor.toFixed(2)
+    }
+}
 
 // Abrir modal
 const abrirModal = () => {
@@ -10,7 +19,7 @@ const abrirModal = () => {
     setTimeout(() => {
         seleciona('.produtoWindowArea').style.opacity = 1
     }, 150)
-};
+}
 
 // FECHAR MODAL
 const fecharModal = () => {
@@ -18,14 +27,14 @@ const fecharModal = () => {
     setTimeout(() => {
         seleciona('.produtoWindowArea').style.display = 'none'
     }, 500)
-};
+}
 
 const botoesFechar = () => {
     // BOTOES FECHAR MODAL
     selecionaTodos('.produtoInfo--cancelButton, .produtoInfo--cancelMobileButton').forEach((item) => {
         item.addEventListener('click', fecharModal)
     })
-};
+}
 
 //abrir e fechar o menu lateral
 seleciona(".hamburguer").addEventListener("click", () =>
@@ -42,19 +51,80 @@ let produtoJson = [
     // {id:6, name:'Biquini rosa - onça', img:'./assets/produtos/biquini-rosa-onça.jpeg', price:58.00, sizes:['P', 'M', 'G'], description:'Produto indicado para formato de corpo ZZZZZZZZZZZ'}
 ];
 
-const preencheDadosDosProdutos = (produtoItem, item) => {
+const getKey = (e) => {
+    // .closest retorna o elemento mais proximo que tem a class que passei
+    let key =  e.target.closest(".card").getAttribute('data-key')
+    
+    // garantir que a quantidade inicial de produtos é 1
+    quantprodutos = 1;
+
+    // Para manter a informação de qual produto foi clicado
+    modalKey = key;
+
+    return key;
+}
+
+const preencherTamanhos = (key) => {
+    // tirar a selecao de tamanho atual e selecionar o tamanho grande
+    seleciona('.produtoInfo--size.selected').classList.remove('selected');
+
+    // selecionar todos os tamanhos
+    selecionaTodos('.produtoInfo--size').forEach((size, sizeIndex) => {
+        // selecionar o tamanho grande
+        (sizeIndex == 2) ? size.classList.add('selected') : '';
+    });
+}
+
+const escolherTamanhoPreco = (key) => {
+    // Ações nos botões de tamanho
+    // selecionar todos os tamanhos
+    selecionaTodos('.produtoInfo--size').forEach((size, sizeIndex) => {
+        size.addEventListener('click', (e) => {
+            // clicou em um item, tirar a selecao dos outros e marca o q vc clicou
+            // tirar a selecao de tamanho atual e selecionar o tamanho grande
+            seleciona('.produtoInfo--size.selected').classList.remove('selected')
+            // marcar o que vc clicou, ao inves de usar e.target use size, pois ele é nosso item dentro do loop
+            size.classList.add('selected')
+
+            // mudar o preço de acordo com o tamanho
+            // seleciona('.produtoInfo--actualPrice').innerHTML = formatoReal(produtoJson[key].price[sizeIndex])
+        })
+    })
+}
+
+const mudarQuantidade = () => {
+    // Ações nos botões + e - da janela modal
+    seleciona('.produtoInfo--qtmais').addEventListener('click', () => {
+        quantprodutos++
+        seleciona('.produtoInfo--qt').innerHTML = quantprodutos
+    });
+
+    seleciona('.produtoInfo--qtmenos').addEventListener('click', () => {
+        if(quantprodutos > 1) {
+            quantprodutos--
+            seleciona('.produtoInfo--qt').innerHTML = quantprodutos	
+        }
+    });
+}
+
+const preencheDadosDosProdutos = (produtoItem, item, index) => {
+    produtoItem.setAttribute('data-key', index);
     produtoItem.querySelector(".card-img-wrapper img").src = item.img;
     produtoItem.querySelector(".produto-item--name h2").innerHTML = item.name;
     produtoItem.querySelector(".produto-item--desc p").innerHTML = item.description;
 
-};
+}
 
 const preencheDadosModal = (item) => {
     seleciona('.produtoBig img').src = item.img;
     seleciona('.produtoInfo h1').innerHTML = item.name;
     seleciona('.produtoInfo--desc').innerHTML = item.description;
-    seleciona('.produtoInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
-};
+    seleciona('.produtoInfo--actualPrice').innerHTML = formatoReal(item.price);
+}
+
+let modalKey = 0;
+let quantProdutos = 1;
+let cart = [];
 
 /************** FIM DECLARAÇÕES ***************/
 
@@ -62,20 +132,34 @@ const preencheDadosModal = (item) => {
 produtoJson.map((item, index) => {
 
     let produtoItem = seleciona(".models .card").cloneNode(true);
-
     seleciona(".produtos").append(produtoItem);
 
     //preencher os dados de cada produto
-    preencheDadosDosProdutos(produtoItem, item);
+    preencheDadosDosProdutos(produtoItem, item, index);
     
-    produtoItem.querySelector('.card-info button').addEventListener("click", () => {
-        
+    produtoItem.querySelector('.card-info button').addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let chave = getKey(e);
+
         // Abrir modal
         abrirModal();
         
         // preenchimento dos dados na modal de forma dinâmica
         preencheDadosModal(item);
+
+        // pegar tamanho selecionado
+        preencherTamanhos(chave);
+        
+        // definir quantidade inicial como 1
+		seleciona('.produtoInfo--qt').innerHTML = quantprodutos
+
+        // selecionar o tamanho e preco(vai mudar o preço de acordo com a numeração??) com o clique no botao
+        escolherTamanhoPreco(chave)
     })
 
     botoesFechar();
 })
+
+// mudar quantidade com os botoes + e -
+mudarQuantidade()
