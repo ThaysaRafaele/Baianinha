400beee374a812fb5c554125d4e3a13700060f73
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-02-12 20:45:03 -0400

    fim

--- script.js
+++ script.js
@@ -290,29 +290,30 @@ const atualizarCarrinho = () => {
 
 const finalizarCompra = () => {
 
-    let total = 0;
-    let message = "Olá, gostaria de fechar o pedido de: ";
-    let pedidoMsg = "";
-    for(let i = 0; i < cart.length; i++){           
-        pedidoMsg = pedidoMsg +  cart[i].Nome +" - "+ cart[i].qt + " unidades" + " - valor: "+ " - tamanho: " + cart[i].size + "%20%20";
-        total += cart[i].qt * cart[i].price;
-    };
-    let totalMsg = "Valor Total do pedido: "+ total;
-        message = message + pedidoMsg + totalMsg;
-        message = message.replace(/\s/g, '%20');
-        // console.log(message)
-    seleciona(".linkFinalizar").href += message;
+
+    
     seleciona('.cart--finalizar').addEventListener('click', () => {
         // console.log('Finalizar compra')
         // console.log(cart.length)
         // console.log(cart)
         
+        let total = 0;
 
-        
+        let message = "Olá, gostaria de fechar o pedido de: ";
+        let pedidoMsg = "";
+        for(let i = 0; i < cart.length; i++){           
+            pedidoMsg = pedidoMsg +  cart[i].Nome +" - "+ cart[i].qt + " unidades" + " - valor: "+ cart[i].price+ " - tamanho: " + cart[i].size + " / ";
+            total += cart[i].qt * cart[i].price;
+        };
+
+        let totalMsg = "Valor Total do pedido: "+ total;
+        message = message + pedidoMsg + totalMsg;
+        message = message.replace(/\s/g, '%20');
+
+        seleciona('.linkFinalizar').href += message;         
 
         console.log(seleciona(".linkFinalizar").href)
 
-        seleciona('.linkFinalizar').href += message; 
         seleciona('.aside-carrinho').classList.remove('show')
         seleciona('.aside-carrinho').style.left = '100vw'
         seleciona('.produtos-header').style.display = 'flex'
